aecc43d21498be9ba0116d2276582fe12db0bfef
Alessadro Esposito Brescia <alessandro.esposito.brescia@gmail.com>
2022-11-14 20:34:44 -0300

    iteration1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
+    <Routes>
+      <Route></Route>
+    </Routes>
     </div>
   );
 }
